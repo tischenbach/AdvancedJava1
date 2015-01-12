d57cfd281851fa79f4592637c87f021c0c4b4be6
Nathanaël <singabrayen.nathanael@gmail.com>
2015-01-12 10:41:04 +0100

    Salut

--- src/advancedjava1/AdvancedJava1.java
+++ src/advancedjava1/AdvancedJava1.java
@@ -16,7 +16,7 @@ public class AdvancedJava1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Bonjour");
+        System.out.println("Salut");
     }
     
 }
