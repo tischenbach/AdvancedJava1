1ce58649efad28aedfcda5b4098d4841e5683ea9
Nathanaël <singabrayen.nathanael@gmail.com>
2015-01-12 10:34:25 +0100

    Test 2

--- src/advancedjava1/AdvancedJava1.java
+++ src/advancedjava1/AdvancedJava1.java
@@ -16,6 +16,7 @@ public class AdvancedJava1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Bonjour");
     }
     
 }
